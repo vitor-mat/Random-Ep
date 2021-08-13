baeab8ad7c6d35c0cc557f36c8ec298f68f84be1
vitor-mat <vitormateusalves123@gmail.com>
2021-08-13 00:30:01 -0300

    mostrnado o numero de temporadas de cada serie na home

--- script/home.js
+++ script/home.js
@@ -2,16 +2,19 @@
 function showStore(){
     const store = JSON.parse(localStorage.getItem("store"))
 
-    const principal = document.getElementById("main-colunm-name")
+    const mainColunmName = document.getElementById("main-colunm-name")
+    const mainColunmSeasons = document.getElementById("main-colunm-seasons")
 
     if(store){
 
         for(let c = 0; c<store.length; c++){
-            const paragrafo = document.createElement("p");
-        
-            paragrafo.innerHTML = store[c].name
-    
-            principal.appendChild(paragrafo)
+            const names = document.createElement("p");
+            names.innerHTML = store[c].name
+            mainColunmName.appendChild(names)
+
+            const seasons = document.createElement("p");
+            seasons.innerHTML = store[c].season
+            mainColunmSeasons.appendChild(seasons)
         }
     }
 }
